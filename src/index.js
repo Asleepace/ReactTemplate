bc1edc829370a6853f1020fce7ed0e72a2f494f4
Colin Teahan <colin_teahan@yahoo.com>
2020-11-11 18:54:45 -0800

    cleaning up files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import reportWebVitals from './utils/reportWebVitals'
-import { App } from './views/app'
+import { App } from './app'
 
 import './assets/css/index.css'
 
